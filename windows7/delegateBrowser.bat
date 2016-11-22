778801f6b7b5048e0a79c07c644c278715fa223c
Tregnaghi, Albert (415) <albert.tregnaghi@daimler.com>
2016-11-22 14:53:30 +0100

    Version 0.2.1
    - url is now inside double quotes, fixes #1
    - debug flag added

--- windows7/delegateBrowser.bat
+++ windows7/delegateBrowser.bat
@@ -1,9 +1,11 @@
 @echo off
+set DEBUG=0
 title DelegateBrowser
 mode 200,40
+if %DEBUG% == 1 (echo called with: 1:%1 2:%2 3:%3 4:%4 5:%5 )
 c:
 cd c:\delegate-browser\windows7
 echo %CD%
 bash ./delegateBrowser.sh %1% &
-rem PAUSE
+if %DEBUG% == 1 (PAUSE)
 exit 0
